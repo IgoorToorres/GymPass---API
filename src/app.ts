e86a98a21ba3a78bb3daace140309105dbe67b64
IgoorToorres <igorevangelista121@gmail.com>
2026-03-24 09:27:34 -0300

    config: configuração incial do banco postgresql, prisma com docker compose

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '../generated/prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Igor Torres',
+    email: 'igorevangelista121@gmail.com',
+  },
+})
