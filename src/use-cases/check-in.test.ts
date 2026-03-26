0e852cfcec15a07d5850aed90db983b6956d8f04
IgoorToorres <igorevangelista121@gmail.com>
2026-03-26 10:25:52 -0300

    feat: usuário não pode fazer check-in se não estiver perto (100m) da academia;

--- src/use-cases/check-in.test.ts
+++ src/use-cases/check-in.test.ts
@@ -19,8 +19,8 @@ describe('Create Check-in', () => {
       title: 'smart',
       description: 'academia boa',
       phone: '',
-      latitude: new Decimal(0),
-      longitude: new Decimal(0),
+      latitude: new Decimal(-15.8665877),
+      longitude: new Decimal(-47.9713124),
     })
 
     vi.useFakeTimers()
@@ -81,5 +81,25 @@ describe('Create Check-in', () => {
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in on distant gym', async () => {
+    gymsRepository.items.push({
+      id: '',
+      title: 'smart',
+      description: 'academia boa',
+      phone: '',
+      latitude: new Decimal(-15.7956138),
+      longitude: new Decimal(-47.8978987),
+    })
+
+    await expect(
+      sut.execute({
+        gymId: 'gym-02',
+        userId: 'user-01',
+        userLatitude: -15.8665877,
+        userLongitude: -47.9713124,
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -2,6 +2,7 @@ import { CheckIn } from 'generated/prisma/client'
 import { CheckInRepository } from '@/repositories/check-ins-repository'
 import { GymsRepository } from '@/repositories/gyms-repository'
 import { ResourceNotFoundError } from './errors/resource-not-found'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-bitween-cordinates'
 
 interface CheckInUseCaseRequest {
   userId: string
@@ -23,11 +24,26 @@ export class CheckInUseCase {
   async execute({
     userId,
     gymId,
+    userLatitude,
+    userLongitude,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
-    const gym = this.gymsRepository.findById(gymId)
+    const gym = await this.gymsRepository.findById(gymId)
 
     if (!gym) {
       throw new ResourceNotFoundError()
+    }
+
+    const distance = getDistanceBetweenCoordinates(
+      { latitude: userLatitude, longitude: userLongitude },
+      {
+        latitude: gym.latitude.toNumber(),
+        longitude: gym.longitude.toNumber(),
+      },
+    )
+    const MAX_DISTANCE_IN_KILOMETERS = 0.1 // 100 metros
+
+    if (distance > MAX_DISTANCE_IN_KILOMETERS) {
+      throw new Error('distancia maior que 100 metros')
     }
 
     const checkInOnSameDay = await this.checkInRepository.findByUserIdOnDate(
