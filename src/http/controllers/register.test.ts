f8e8f76669c4ac9f3fa17ac5c4c5d2f14075c585
IgoorToorres <igorevangelista121@gmail.com>
2026-03-27 19:33:15 -0300

    test: test e2e para registrar usuario

--- src/http/controllers/register.test.ts
+++ src/http/controllers/register.test.ts
@@ -1,5 +1,23 @@
-import { expect, test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('testing', () => {
-  expect(1 + 1).toEqual(2)
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('shloud be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'fulano',
+      email: 'fulano@gmail.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
