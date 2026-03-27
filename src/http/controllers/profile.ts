1bc2fdc8658708f3904534f7f9e12443181608d2
IgoorToorres <igorevangelista121@gmail.com>
2026-03-27 17:00:36 -0300

    feat: controller de profile com usuario authenticado

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,8 +1,15 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyRequest, FastifyReply } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
+  const userId = request.user.sub
+  const getUserProfile = makeGetUserProfileUseCase()
+  const { user } = await getUserProfile.execute({ userId })
 
-  console.log(request.user.sub)
-  return reply.status(200).send()
+  return reply.status(200).send({
+    profile: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
