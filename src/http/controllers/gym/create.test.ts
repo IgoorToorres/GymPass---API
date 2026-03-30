f42dee8d235615e24088772aac221d7a29811394
IgoorToorres <igorevangelista121@gmail.com>
2026-03-30 10:16:42 -0300

    test: testen e2e de create gym

---
+++ src/http/controllers/gym/create.test.ts
@@ -0,0 +1,31 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Create gym (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('shloud be able to create a gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'test-1',
+        description: 'first test',
+        phone: '61123123',
+        latitude: -15.8665877,
+        longitude: -47.9713124,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
