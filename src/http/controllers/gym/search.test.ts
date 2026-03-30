64177e38347741f5c8b3f1dc6b679fa2bc18f68c
IgoorToorres <igorevangelista121@gmail.com>
2026-03-30 10:17:23 -0300

    teste: teste e2e de buscar academias por query pelo nome da academia

---
+++ src/http/controllers/gym/search.test.ts
@@ -0,0 +1,56 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Search Gym (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('shloud be able to search a gym, by name', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'academia longe',
+        description: 'first test',
+        phone: '61123123',
+        latitude: -15.8665877,
+        longitude: -47.9713124,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'academia perto',
+        description: 'first test',
+        phone: '61123123',
+        latitude: -15.8665877,
+        longitude: -47.9713124,
+      })
+
+    const gyms = await request(app.server)
+      .get('/gyms/search')
+      .set('Authorization', `Bearer ${token}`)
+      .query({
+        q: 'perto',
+        page: 1,
+      })
+
+    expect(gyms.statusCode).toEqual(200)
+    expect(gyms.body.gyms).toHaveLength(1)
+    expect(gyms.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'academia perto',
+      }),
+    ])
+  })
+})
